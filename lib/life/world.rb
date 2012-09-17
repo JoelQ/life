c8034eed75013e7fe84064fe2cc81bcff7c5d112
Joel Quenneville <joel.quenneville@collegeplus.org>
2012-09-17 14:09:54 -0500

    add ability to seed initial world

--- lib/life/world.rb
+++ lib/life/world.rb
@@ -2,7 +2,12 @@ class World
 
   attr_reader :current
 
-  def initialize(x, y)
+  def initialize(x, y, seed = [])
     @current = Array.new(x, Array.new(y, Cell.new(false)))
+    seed.each do |cell_coords|
+      x = cell_coords[0]
+      y = cell_coords[1]
+      current[x][y].live
+    end
   end
 end
--- spec/life/world_spec.rb
+++ spec/life/world_spec.rb
@@ -13,4 +13,26 @@ describe World do
       world.current[0][1].live?.should be_false
     end
   end
+
+  describe "seed pattern" do
+    let(:world) { World.new(4,5, [[0,0], [1,2], [3,1]])}
+    it "should be initialized correctly" do
+      world.current[0][0].should be_live
+      world.current[1][2].should be_live
+      world.current[3][1].should be_live
+    end
+  end
+
+  describe "Rules" do
+    describe "Any live cell with fewer than two live neighbours dies, as if caused by under-population" do
+      context "when no neighbours" do
+        let(:world) { World.new(3,3, [[1,1]]) }
+        it "NW cell should be dead " do
+          world.tick
+          world.current[1][1].should be_dead
+        end
+      end
+
+    end
+  end
 end
