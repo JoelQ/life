0624ff7e3892e8920c31ac510c3abf101225c151
Joel Quenneville <joel.quenneville@collegeplus.org>
2012-09-20 15:53:08 -0500

    speed up animation

--- lib/life/cli.rb
+++ lib/life/cli.rb
@@ -54,7 +54,7 @@ module Life
         def display_simulation(max_gen, height, world)
           (1..max_gen).each do |gen|
             print world.to_s("@", " ") + eol(height, gen, max_gen)
-            sleep(1)
+            sleep(0.5)
             world.tick
           end
         end
