903ba7674b80771e7bb6a264da3f0b6a5f0c3328
Joel Quenneville <joel.quenneville@collegeplus.org>
2012-09-20 14:16:20 -0500

    prevent bad data from being passed in

--- lib/life/cli.rb
+++ lib/life/cli.rb
@@ -24,18 +24,39 @@ module Life
         end
       end
 
-      def eol(height, curr_gen, max_gen)
-        curr_gen == max_gen ? "\n" : format("\e[1A" * height + "\r")
-      end
-
-      def build_world(width, height)
-        seed = []
-        while yes? "Would you like to add a live cell to the grid? [Y/n]"
-          x = ask "Please enter x coordinate"
-          y = ask "Please enter y coordinate"
-          seed << [(x.to_i) - 1, (y.to_i) -1]
+      no_tasks do
+        def eol(height, curr_gen, max_gen)
+          curr_gen == max_gen ? "\n" : format("\e[1A" * height + "\r")
         end
-        World.new width, height, seed
+
+        def build_world(width, height)
+          seed = []
+          while yes? "Would you like to add a live cell to the grid? [Y/n]"
+            x = get_x(width)
+            y = get_y(height)
+            seed << [(x.to_i) - 1, (y.to_i) -1]
+          end
+          World.new width, height, seed
+        end
+
+        def get_x(width)
+          x = ask("Please enter x coordinate").to_i
+          while x > width
+            puts "This number is too big"
+            x = ask("Please enter x coordinate").to_i
+          end
+          return x
+        end
+
+        def get_y(height)
+          y = ask("Please enter y coordinate").to_i
+          while y > height
+            puts "This number is too big"
+            y = ask("Please enter y coordinate").to_i
+          end
+          return y
+        end
+
       end
 
     end
