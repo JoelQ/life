045d34d2532a6f6c58bbd9e413bbf8ef64d7751a
Joel Quenneville <joel.quenneville@collegeplus.org>
2012-09-20 15:52:30 -0500

    change dead cell symbol to empty space

--- lib/life/cli.rb
+++ lib/life/cli.rb
@@ -53,7 +53,7 @@ module Life
 
         def display_simulation(max_gen, height, world)
           (1..max_gen).each do |gen|
-            print world.to_s("@", "_") + eol(height, gen, max_gen)
+            print world.to_s("@", " ") + eol(height, gen, max_gen)
             sleep(1)
             world.tick
           end
